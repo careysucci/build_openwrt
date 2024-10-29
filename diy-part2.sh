aad99aa2ee59810528bbdf4abfee555d5beda4f8
careysucci <10008359+lvbenwei_10086@user.noreply.gitee.com>
2024-10-29 15:55:44 +0800

    fix bugs

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,25 +10,20 @@
 # See /LICENSE for more information.
 #
 
-# enter openwrt folder
-pushd openwrt || exit
 
 # switch theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
 
-cp -f diy/common/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+cp -f diy/common/zzz-default-settings openwrt/package/lean/default-settings/files/zzz-default-settings
 # banner
-cp -f diy/banner package/base-files/files/etc/banner
-sed -i "s/%D %V, %C/openwrt $(date +'%m.%d') by ${AUTHORED_BY}/g" package/base-files/files/etc/banner
+cp -f diy/banner openwrt/package/base-files/files/etc/banner
+sed -i "s/%D %V, %C/openwrt $(date +'%m.%d') by ${AUTHORED_BY}/g" openwrt/package/base-files/files/etc/banner
 
 # Modify default IP
-sed -i 's/192.168.1.1/172.16.3.18/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/172.16.3.18/g' openwrt/package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/Wy.House/g' package/base-files/files/bin/config_generate
-
-# back to root folder
-popd || exit
+sed -i 's/OpenWrt/Wy.House/g' openwrt/package/base-files/files/bin/config_generate
