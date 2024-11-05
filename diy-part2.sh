7e55989cd7367737ff21f23a8a917423f43198c3
careysucci <10008359+lvbenwei_10086@user.noreply.gitee.com>
2024-11-05 20:19:17 +0800

    fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,28 +10,25 @@
 # See /LICENSE for more information.
 #
 
-#
-echo "OPENWRT_SOURCE_DIR = ${TARGET_MATRIX}" >> $GITHUB_ENV
-
 
 # clear small duplicate packages
-rm -rf "${OPENWRT_SOURCE_DIR}"/feeds/small/luci-app-passwall
-rm -rf "${OPENWRT_SOURCE_DIR}"/feeds/small/luci-app-passwall2
-rm -rf "${OPENWRT_SOURCE_DIR}"/feeds/small/luci-app-openclash
-#rm -rf "${OPENWRT_SOURCE_DIR}"/feeds/small/luci-app-homeproxy
-rm -rf "${OPENWRT_SOURCE_DIR}"/feeds/small/mihomo
-rm -rf "${OPENWRT_SOURCE_DIR}"/feeds/small/luci-app-mihomo
+rm -rf "${TARGET_MATRIX}"/feeds/small/luci-app-passwall
+rm -rf "${TARGET_MATRIX}"/feeds/small/luci-app-passwall2
+rm -rf "${TARGET_MATRIX}"/feeds/small/luci-app-openclash
+#rm -rf "${TARGET_MATRIX}"/feeds/small/luci-app-homeproxy
+rm -rf "${TARGET_MATRIX}"/feeds/small/mihomo
+rm -rf "${TARGET_MATRIX}"/feeds/small/luci-app-mihomo
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' "${OPENWRT_SOURCE_DIR}"/feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' "${TARGET_MATRIX}"/feeds/luci/collections/luci/Makefile
 
-cp -f diy/common/zzz-default-settings "${OPENWRT_SOURCE_DIR}"/package/lean/default-settings/files/zzz-default-settings
+cp -f diy/common/zzz-default-settings "${TARGET_MATRIX}"/package/lean/default-settings/files/zzz-default-settings
 # banner
-cp -f diy/banner "${OPENWRT_SOURCE_DIR}"/package/base-files/files/etc/banner
-sed -i "s/%D %V, %C/OpenWrt by ${AUTHORED_BY} $(date +'%Y-%m-%d')/g" "${OPENWRT_SOURCE_DIR}"/package/base-files/files/etc/banner
+cp -f diy/banner "${TARGET_MATRIX}"/package/base-files/files/etc/banner
+sed -i "s/%D %V, %C/OpenWrt by ${AUTHORED_BY} $(date +'%Y-%m-%d')/g" "${TARGET_MATRIX}"/package/base-files/files/etc/banner
 
 # Modify default IP
-sed -i 's/192.168.1.1/172.16.3.18/g' "${OPENWRT_SOURCE_DIR}"/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/172.16.3.18/g' "${TARGET_MATRIX}"/package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i "s/OpenWrt/${AUTHORED_BY}/g" "${OPENWRT_SOURCE_DIR}"/package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt/${AUTHORED_BY}/g" "${TARGET_MATRIX}"/package/base-files/files/bin/config_generate
