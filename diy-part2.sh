7531e53cc439a124cb100c8d07967ec9e60ed6d5
wy <ccc@gitee.com>
2024-11-08 23:44:37 +0800

    fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,10 +30,26 @@ sed -i "s/%D %V, %C/OpenWrt by ${AUTHORED_BY} $(date +'%Y-%m-%d')/g" "${TARGET_M
 pushd "${TARGET_MATRIX}" || exit
 short_commit_id=$(git rev-parse --short HEAD)
 popd || exit
-sed -i "s/%D/WyWrt/g" "${TARGET_MATRIX}"/package/base-files/files/etc/openwrt_release
+sed -i "s/%D/${RELEASE_NAME}/g" "${TARGET_MATRIX}"/package/base-files/files/etc/openwrt_release
 sed -i "s/%V/${DATE4}/g" "${TARGET_MATRIX}"/package/base-files/files/etc/openwrt_release
 sed -i "s/%C/git-${short_commit_id}/g" "${TARGET_MATRIX}"/package/base-files/files/etc/openwrt_release
-sed -i "s/Openwrt/Wywrt/g" "${TARGET_MATRIX}"/package/base-files/files/etc/openwrt_release
+sed -i "s/Openwrt/${RELEASE_NAME}/g" "${TARGET_MATRIX}"/package/base-files/files/etc/openwrt_release
+# os-release
+sed -i "s/%D/${RELEASE_NAME}/g" "${TARGET_MATRIX}"/package/base-files/files/usr/lib/os-release
+sed -i "s/%V/${DATE4}/g" "${TARGET_MATRIX}"/package/base-files/files/usr/lib/os-release
+sed -i "s/%C/git-${short_commit_id}/g" "${TARGET_MATRIX}"/package/base-files/files/usr/lib/os-release
+# mkversion
+case ${TARGET_MATRIX} in
+  'lede')
+    sed -i "s/\${2:-Git}/${short_commit_id}/g" "${TARGET_MATRIX}"/package/feeds/luci/luci-lua-runtime/src/mkversion.sh
+    ;;
+  'official')
+  # luci name
+#    sed -i "s/\$\{3:-LuCI\}//g" "${TARGET_MATRIX}"/feeds/base/feeds/luci/luci-lua-runtime/src/mkversion.sh
+    # luci version
+    sed -i "s/\${2:-Git}/${short_commit_id}/g" "${TARGET_MATRIX}"/feeds/base/feeds/luci/luci-lua-runtime/src/mkversion.sh
+    ;;
+esac
 
 # Modify default IP
 sed -i 's/192.168.1.1/172.16.3.18/g' "${TARGET_MATRIX}"/package/base-files/files/bin/config_generate
