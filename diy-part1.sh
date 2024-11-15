f7c8e3fbfe81c7390cdf7cc493c1160f8eaa7bb0
careysucci <10008359+lvbenwei_10086@user.noreply.gitee.com>
2024-11-15 10:30:38 +0800

    fix

--- diy-part1.sh
+++ diy-part1.sh
@@ -47,8 +47,8 @@ sed -i "/helloworld/d" "feeds.conf.default"
   echo "src-git diskman https://github.com/careysucci/luci-app-diskman.git;master"
   echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall'
   echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2'
-  echo "src-git OpenClash https://github.com/vernesong/OpenClash.git;master"
-  echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main"
+#  echo "src-git OpenClash https://github.com/vernesong/OpenClash.git;master"
+#  echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main"
 } >> "feeds.conf.default"
 
 # back to root folder
